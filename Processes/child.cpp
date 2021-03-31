f861699f049ebba847ad3b6c14a17e96b24df096
illinois1204 <50563308+illinois1204@users.noreply.github.com>
2021-03-31 23:35:58 +0700

    Add files via upload

---
+++ Processes/child.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <map>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string INPUT_FILE = argv[0];
+    const int OFFSET = stoi(argv[1]);
+    const int FRAGMENT_SIZE = stoi(argv[2]);
+    string OUTPUT_FILE = argv[3];
+
+    string Text, row;
+    ifstream fin;
+	ofstream fout;
+    fin.open(INPUT_FILE);
+    if (!fin.is_open()) {
+        cout << "Error to open file" << endl;
+        exit(1);
+    }
+    while (getline(fin, row)) 
+        Text += row;
+    fin.close();
+
+    Text = Text.substr(OFFSET, FRAGMENT_SIZE);
+    transform(Text.begin(), Text.end(), Text.begin(), ::tolower);
+
+    int frequency;
+    map<string, int> Trigramms;
+
+    for (int i = 1; i < Text.length() - 1; i++) 
+    {
+        if (isalpha(Text[i-1]) && isalpha(Text[i]) && isalpha(Text[i+1]) )  
+        {
+            string trio = string(1, Text[i - 1]) + string(1, Text[i]) + string(1, Text[i + 1]);
+            auto it = Trigramms.find(trio);
+            it == Trigramms.end() ? frequency = 1 : frequency = it->second+1;
+            Trigramms[trio] = frequency;
+        }
+    }
+
+	fout.open(OUTPUT_FILE);
+    fout << "Fragment of text:" << endl<<Text<<endl<<endl;
+    fout << "Quantity of Trigramms: " << Trigramms.size() << endl<<endl;
+    for(auto it : Trigramms)
+        fout << "Trig: " << it.first << "\tFrequency: "<< it.second << endl;
+	fout.close();
+    exit(0);
+}
---
+++ Processes/parent.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+using namespace std;
+
+void Processing(string File, int FragmentSize, int OperationNumb);
+int fileSize(string path_to_file) {
+	ifstream file(path_to_file, ios::binary | ios::ate);
+	return file.tellg();
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc < 3){
+        cout << "Wrong parameters count!\nCancel" << endl;
+        exit(1);
+    }
+    cout << "Main programm with pid = " << getpid()<<endl;
+	string INPUT_FILE = argv[1];
+	const int DIVIDER = stoi(argv[2]);
+
+	int FRAGMENT_SIZE = fileSize(INPUT_FILE) / DIVIDER;
+
+	for (int i = 0; i < DIVIDER; i++) 
+		Processing(INPUT_FILE, FRAGMENT_SIZE, i);
+
+	return 0;
+}
+
+void Processing(string File, int FragmentSize, int OperationNumb) {
+	pid_t pid = fork();
+	if(pid < 0) {
+		cout << "Error to run procces " << OperationNumb+1 << endl;
+	 	return;
+	}
+	if(pid == 0) {
+		int offset = OperationNumb * FragmentSize;
+		string OUTPUT_FILE = "data_operation_"+to_string(OperationNumb+1)+".txt";
+		char *parameters[] = {
+			(char*)File.c_str(),
+			(char*)to_string(offset).c_str(),
+			(char*)to_string(FragmentSize).c_str(),
+			(char*)OUTPUT_FILE.c_str(),
+			NULL
+		};
+		cout << "Run " << OperationNumb + 1 << " process with pid = "<< getpid()<<endl;
+		execv("child", parameters);
+	}
+	// else {
+	    // int code;
+        // wait(&code);
+        // cout<<"Child process "<<OperationNumb+1<<" ended with code " << WEXITSTATUS(code) << endl;
+	// }
+}
+
